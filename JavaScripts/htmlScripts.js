108568edf2575ac1ce52edd8da9aea3abdbb551f
Trong Tan <trongtan2104@yahoo.com>
2024-11-06 17:38:47 +0700

    project_web

---
+++ JavaScripts/htmlScripts.js
@@ -0,0 +1,5 @@
+$('#Header').load('./Sidehtml/header.html');
+$('#Footer').load('./Sidehtml/footer.html');
+$('#side-bar').load('./Sidehtml/sidebar.html');
+$('#usermodalbox').load('./Sidehtml/loginpage.html')
+$('#usermodalbox').load('./Sidehtml/registerpage.html')
